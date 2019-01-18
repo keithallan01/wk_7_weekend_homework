ff6305a71b5b56c2ea7be49e1c2df32b86359ecd
Keith Allan <keithallan01@gmail.com>
2019-01-18 15:34:38 +0000

    struggling to getData

---
+++ src/models/quotes.js
@@ -0,0 +1,24 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Quotes = function() {
+  this.data = [];
+};
+
+Quotes.prototype.getData = function () {
+  const url = `https://seinfeld-quotes.herokuapp.com/quotes`;
+  const request = new RequestHelper(url);
+
+  const myPromise = request.get();
+
+    myPromise.then((data) => {
+      this.data = data.quotes[];
+      console.log(this.data);
+      PubSub.publish('Quotes:data-ready', this.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+};
+
+module.exports = Quotes;
